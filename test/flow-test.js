fa0030066d9a5999d2a2e16b245c2a52d2648f28
vetoshko <vetoshkodasha@gmail.com>
2016-03-30 20:18:31 +0500

    async

--- test/flow-test.js
+++ test/flow-test.js
@@ -6,104 +6,167 @@ const sinon = require('sinon');
 
 describe('Flow', () => {
     describe('serial', () => {
-        it ('should call callback with error if fail', () => {
-            var spy = sinon.spy((error, data) => {});
+        it ('should call callback with error if fail', done => {
+            var spy = sinon.spy((error, data) => {
+                sinon.assert.calledWith(spy, 'error');
+                done();
+            });
             var errorFunction = (callback) => {
-                callback('error', 'data');
+                setTimeout(callback, 30, 'error', 'data');
             };
             flow.serial([errorFunction], spy);
-            assert.ok(spy.calledWith('error'));
         });
-        it('should call callback when array ends', () => {
-            var spy = sinon.spy((error, data) => {});
-            var lastFunction = (callback) => {
-                callback(null, 'data')
-            };
+        it('should call callback when array ends', done => {
+            var spy = sinon.spy((error, data) => {
+                assert.ok(spy.calledAfter(lastFunction));
+                done();
+            });
+            var lastFunction = sinon.spy((callback) => {
+                setTimeout(callback, 20, null, 'data')
+            });
             flow.serial([lastFunction], spy);
-            assert.ok(spy.calledWith(null, 'data'));
+
         });
-        it('should call next function with result of previous function', () => {
-            var spy = sinon.spy((error, data) => {});
+        it('should call next function with result of previous function', done => {
+            var spy = sinon.spy((error, data) => {
+                assert.ok(nextFunction.calledWith('previousData'));
+                done();
+            });
             var previousFunction = (callback) => {
-                callback(null, 'previousData');
+                setTimeout(callback, 30, null, 'previousData');
             };
             var nextFunction = sinon.spy((data, callback) => {
-                callback(null, 'result');
+                setTimeout(callback, 20, null, 'result');
             });
             flow.serial([previousFunction, nextFunction], spy);
-            assert.equal(spy.callCount, 1);
-            assert.ok(nextFunction.calledWith('previousData'));
         });
-        it('should call global callback once no matter what (I got your back)', () => {
-            var spy = sinon.spy((error, data) => {});
+        it('should call global callback once no matter what', done => {
+            var spy = sinon.spy((error, data) => {
+                assert.ok(spy.calledOnce);
+                done();
+            });
             var previousFunction = (callback) => {
-                callback('firstError', 'previousData');
+                setTimeout(callback, 40, 'firstError', 'previousData');
             };
             var nextFunction = sinon.spy((data, callback) => {
-                callback('secondError', 'result');
+                setTimeout(callback, 5, 'secondError', 'result');
             });
             flow.serial([previousFunction, nextFunction], spy);
-            assert.equal(spy.callCount, 1);
+        });
+        it('shouldn\'t call next function if previous ends with error', done => {
+            var spy = (error, data) => {
+                assert.equal(nextFunction.callCount, 0);
+                done();
+            };
+            var functionWithError = sinon.spy((callback) => {
+                setTimeout(callback, 20, 'firstError', 'previousData');
+            });
+            var nextFunction = sinon.spy((data, callback) => {
+                setTimeout(callback, 10, 'secondError', 'result');
+            });
+            flow.serial([functionWithError, nextFunction], spy);
+        });
+        it('shouldn call second function after first', done => {
+            var spy = (error, data) => {
+                assert.ok(secondFunction.calledAfter(firstFunction));
+                done();
+            };
+            var firstFunction = sinon.spy((callback) => {
+                setTimeout(callback, 35, null, 'previousData');
+            });
+            var secondFunction = sinon.spy((data, callback) => {
+                setTimeout(callback, 37, null, 'result');
+            });
+            flow.serial([firstFunction, secondFunction], spy);
         })
     });
     describe('parallel', () => {
-        it('should call global callback once', () => {
-            var spy = sinon.spy((error, data) => {});
-            var firstFunction = (callback) => {
-                callback(null, 'firstData');
-            };
-            var secondFunction = (callback) => {
-                callback(null, 'secondData');
-            };
-            flow.parallel([firstFunction, secondFunction], spy);
-            assert.ok(spy.calledOnce);
-        });
-        it('should call global callback once if errors', () => {
-            var spy = sinon.spy((error, data) => {});
-            var firstFunction = (callback) => {
-                callback('firstError', 'firstData');
-            };
-            var secondFunction = (callback) => {
-                callback('secondError', 'secondData');
-            };
-            flow.parallel([firstFunction, secondFunction], spy);
-            assert.ok(spy.calledOnce);
-        });
-        it('should have all results', () => {
+        it('should call global callback once', done => {
             var spy = sinon.spy((error, data) => {
+                assert.ok(spy.calledOnce);
+                done();
+            });
+            var firstFunction = (callback) => {
+                setTimeout(callback, 9, null, 'firstData');
+            };
+            var secondFunction = (callback) => {
+                setTimeout(callback, 5, null, 'secondData');
+            };
+            flow.parallel([firstFunction, secondFunction], spy);
+        });
+        it('should call global callback once if errors', done => {
+            var spy = sinon.spy((error, data) => {
+                assert.ok(spy.calledOnce);
+                done();
+            });
+            var firstFunction = (callback) => {
+                setTimeout(callback, 40, 'firstError', 'firstData');
+            };
+            var secondFunction = (callback) => {
+                setTimeout(callback, 25, 'secondError', 'secondData');
+            };
+            flow.parallel([firstFunction, secondFunction], spy);
+        });
+        it('should have all results', done => {
+            var spy = sinon.spy((error, data) => {
+                assert.ok(spy.withArgs(null, 'firstData'));
+                assert.ok(spy.withArgs(null, 'secondData'));
+                done();
                 return data;
             });
             var firstFunction = (callback) => {
-                callback('firstError', 'firstData');
+                setTimeout(callback, 4, null, 'firstData');
             };
             var secondFunction = (callback) => {
-                callback('secondError', 'secondData');
+                setTimeout(callback, 13, null, 'secondData');
             };
             flow.parallel([firstFunction, secondFunction], spy);
-            assert.equal(spy.length, 2);
         });
     });
     describe('map', () => {
-        it('should call with all arguments', () => {
+        it('should call with all arguments', done => {
             var mapFunction = sinon.spy((argument) => {
-                return argument;
+                setTimeout(function(argument) {
+                    return argument;
+                }, 4)
             });
-            var spy = sinon.spy((error, data) => {});
+            var spy = sinon.spy((error, data) => {
+                assert.ok(mapFunction.calledWith(4));
+                assert.ok(mapFunction.calledWith(8));
+                assert.ok(mapFunction.calledWith(15));
+                assert.ok(mapFunction.calledWith(16));
+                assert.ok(mapFunction.calledWith(23));
+                assert.ok(mapFunction.calledWith(42));
+                done();
+            });
             flow.map([4, 8, 15, 16, 23, 42], mapFunction, spy);
-            assert.ok(mapFunction.calledWith(4));
-            assert.ok(mapFunction.calledWith(8));
-            assert.ok(mapFunction.calledWith(15));
-            assert.ok(mapFunction.calledWith(16));
-            assert.ok(mapFunction.calledWith(23));
-            assert.ok(mapFunction.calledWith(42));
         });
-        it('should call global callback once', () => {
-            var spy = sinon.spy((error, data) => {});
+        it('should call global callback once', done => {
+            var spy = sinon.spy((error, data) => {
+                assert.equal(spy.callCount, 1);
+                done();
+            });
             var mapFunction = (argument) => {
-                return argument
+                setTimeout(function(argument) {
+                    return argument;
+                }, 5);
             };
             flow.map([4, 8, 15, 16, 23, 42], mapFunction, spy)
-            assert.equal(spy.callCount, 1);
+        });
+        it('should call global callback once if error', done => {
+            var spy = sinon.spy((err, data) => {
+                if (err) {
+                    console.error(err);
+                }
+                assert.equal(spy.callCount, 1);
+                done();
+            });
+            var mapFunction = (argument) => {
+                setTimeout(function(argument) {
+                    return('error', argument)
+                }, 20);
+            };
+            flow.map([4, 8, 15, 16, 23, 42], mapFunction, spy)
         });
     });
 });
